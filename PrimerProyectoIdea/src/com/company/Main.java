dfc9801a66e8c50e96e7385123b2c1c4c8d9f3c3
Ignacio Moreno <Ignacio.moreno.gomez2003@gmail.com>
2021-11-30 09:49:39 +0100

    first commit2

--- PrimerProyectoIdea/src/com/company/Main.java
+++ PrimerProyectoIdea/src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Hola mundo");;
+        System.out.println("Hola mundo");
+        System.out.println("Adios mundo");
         Alumno a1 =new Alumno("Nacho","Moreno");
     }
 
