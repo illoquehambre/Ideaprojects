b05d6ee361b03f5046ad2aa000092416c201c80f
Ignacio Moreno <Ignacio.moreno.gomez2003@gmail.com>
2021-11-30 09:45:49 +0100

    first commit

---
+++ PrimerProyectoIdea/src/com/company/Alumno.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Alumno {
+    private String nombre;
+    private String apellido;
+
+    public Alumno(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido= apellido;
+    }
+}
