b9c3f9a8bce62bac168c5e30242b475a8fc3f7fd
Ignacio Moreno <Ignacio.moreno.gomez2003@gmail.com>
2021-12-02 09:54:24 +0100

    cambios to wapos

--- Merges/src/com/company/Main.java
+++ Merges/src/com/company/Main.java
@@ -4,14 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int a=24, suma=0;
         int [] array =new int[3];
-        array[0]=76;
-        array[1]=254;
-        array[2]=471;
-
-        System.out.println(array[0]);
-        System.out.println(array[1]);
-        System.out.println(array[2]);
+       for (int i=0;i<array.length;i++){
+           suma=a+suma;
+           array[i]=suma;
+           System.out.println(array[i]);
+       }
 
     }
 }
