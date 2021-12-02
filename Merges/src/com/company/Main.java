21c9b391bb96fe5c85dff366ffed358d6fc27ce8
Ignacio Moreno <Ignacio.moreno.gomez2003@gmail.com>
2021-12-02 09:33:36 +0100

    hey hey

--- Merges/src/com/company/Main.java
+++ Merges/src/com/company/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
+        System.out.println("nvckjrw vrnwvcnrwoivn" );
+        System.out.println("cvkjwnvujewifouwhj9gf");
+        System.out.println("bfhiebcnwcnreiwvnurw");
+        System.out.println("bfhbcuwevcnurwnvjuirwuvf");
     }
 }
